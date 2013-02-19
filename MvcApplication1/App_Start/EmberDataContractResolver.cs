e7848c6e298f2861aae333b447113d525f6625cc
Thiago Santos <thiago_trds@outlook.com>
2013-02-19 15:51:57 -0500

    will definitely delete this

---
+++ MvcApplication1/App_Start/EmberDataContractResolver.cs
@@ -0,0 +1,38 @@
+﻿// Inspired by Gist https://gist.github.com/roryf/1042502#file-deliminatorseparatedpropertynamescontractresolver-cs
+
+using System.Text;
+using System.Collections.Generic;
+using Newtonsoft.Json.Serialization;
+
+
+public class DeliminatorSeparatedPropertyNamesContractResolver : DefaultContractResolver {
+    private readonly string _separator;
+
+    protected DeliminatorSeparatedPropertyNamesContractResolver(char separator = '_')
+        : base(true) {
+        _separator = separator.ToString();
+    }
+
+    protected override string ResolvePropertyName(string propertyName) {
+        var parts = new List<string>();
+        var currentWord = new StringBuilder();
+
+        foreach (var c in propertyName) {
+            if (char.IsUpper(c) && currentWord.Length > 0) {
+                parts.Add(currentWord.ToString());
+                currentWord.Clear();
+            }
+            currentWord.Append(char.ToLower(c));
+        }
+
+        if (currentWord.Length > 0) {
+            parts.Add(currentWord.ToString());
+        }
+
+        return string.Join(_separator, parts.ToArray());
+    }
+}
+
+public class EmberDataContractResolver : DeliminatorSeparatedPropertyNamesContractResolver {
+    public EmberDataContractResolver() : base('_') { }
+}
---
+++ MvcApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
+namespace MvcApplication1.Controllers {
+
+    public class Person {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class ValuesController : ApiController {
+        // GET api/values
+        public object Get() {
+            var chararacters = new List<Person>() { 
+                new Person() {
+                    Id = 1,
+                    FirstName = "Hiro",
+                    LastName = "Nakamura"
+                },
+                new Person() {
+                    Id = 2,
+                    FirstName = "Clark",
+                    LastName = "Kent"
+                },
+                new Person() {
+                    Id = 3,
+                    FirstName = "Peter",
+                    LastName = "Parker"
+                }
+            };
+            return new { people = chararacters };
+        }
+
+        // GET api/values/5
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value) {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value) {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id) {
+        }
+    }
+}
